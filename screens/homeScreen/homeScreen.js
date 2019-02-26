7b34bce23c73f76cc07525754c1f6772c5e26dbb
NKonstantin007 <kostya.1998.nosarev@mail.ru>
2019-02-27 05:16:50 +0700

    Hiding the signup button on the main page if the user is signin

--- screens/homeScreen/homeScreen.js
+++ screens/homeScreen/homeScreen.js
@@ -1,5 +1,9 @@
 'use strict'
 
-angular.module("controllers").controller("homeScreenController", function($scope) {
+angular.module("controllers").controller("homeScreenController", function($scope, userModel) {
+	function init() {
+		$scope.showSignupBtn = userModel.isAuth();
+	}
 
+	init();
 });
